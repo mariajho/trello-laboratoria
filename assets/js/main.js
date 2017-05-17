9b511a4aec141e136eb33779e0c5f3c7daa7657d
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-17 14:27:17 -0400

    avance

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/*function añadir(){
+	var lista=document.getElementById("lista").value;
+	document.getElementById("tareas").value="";
+}
+
+var contenedor = document.getElementById("contenedor").value;
+var cont=document.getElementById("contenedor").value;
+
+function retornarLista(){
+	var imprimir=document.getElementById("lista").inner.HTML
+}*/
+
